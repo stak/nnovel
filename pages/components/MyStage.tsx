654a9963b33682d3f70ca2fb5ed101d1098cae87
Shutaro Takimoto <shu@tak.im>
2020-03-29 17:28:48 +0900

    feat: グローバルスタイルあてた

--- pages/components/MyStage.tsx
+++ pages/components/MyStage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Stage, Sprite } from '@inlet/react-pixi'
+import { NextComponentType, NextPageContext } from 'next'
 
 const stageOption = {
   width: 800,
@@ -10,7 +11,7 @@ const stageOption = {
 }
 
 type Props = {}
-const MyStage: React.FC<Props> = () => (
+const MyStage: NextComponentType<NextPageContext, {}, Props> = () => (
   <Stage options={stageOption}>
     <Sprite image="/img/bg1.png" x={0} y={0} />
     <Sprite image="/img/arie.png" x={100} y={0} />
