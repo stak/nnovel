caddfa9749159cd0105f96a8669c6e327968d9bf
Shutaro Takimoto <shu@tak.im>
2020-03-29 15:57:48 +0900

    chore: setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    "react/prop-types": "off", // handled by TypeScript
+  }
+}
